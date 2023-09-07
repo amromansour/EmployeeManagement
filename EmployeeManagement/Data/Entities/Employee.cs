8355058da0fe2316d862ffa4e29fdce459199545
Amro Mansour <amro.mansour@suezcem.com>
2023-09-07 13:08:35 +0300

    db cONNECTION

--- EmployeeManagement/Data/Entities/Employee.cs
+++ EmployeeManagement/Data/Entities/Employee.cs
@@ -1,7 +1,25 @@
-﻿namespace EmployeeManagement.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagement.Data.Entities
 {
     public class Employee
     {
-        public Guid MyProperty { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
+        public int Age { get; set; }
+
+        public string FirstName { get; set; }
+       
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public Guid DepartmentId { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        public Department Departments { get; set;}
+     
+         
+
     }
 }
--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,3 +1,10 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DB Connection String
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
